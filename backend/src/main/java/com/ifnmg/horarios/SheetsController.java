3ef9c8782d08e8f4c612483244333a3127c379fb
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2024-09-30 13:27:15 -0300

    Correções no projeto para organização e segurança

--- backend/src/main/java/com/ifnmg/horarios/SheetsController.java
+++ backend/src/main/java/com/ifnmg/horarios/SheetsController.java
@@ -20,7 +20,7 @@ import java.util.stream.IntStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "${FRONTEND_URL}")
 @RestController
 public class SheetsController {
 
--- backend/src/test/java/com/ifnmg/horarios/HorariosApplicationTests.java
+++ backend/src/test/java/com/ifnmg/horarios/HorariosApplicationTests.java
@@ -1,10 +1,19 @@
 package com.ifnmg.horarios;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HorariosApplicationTests {
+
+	// Carregar variáveis de ambiente antes de fazer os testes
+	@BeforeAll
+    public static void setUp() {
+        System.setProperty("API_KEY", System.getenv("API_KEY"));
+        System.setProperty("SPREADSHEET_ID", System.getenv("SPREADSHEET_ID"));
+        System.setProperty("FRONTEND_URL", System.getenv("FRONTEND_URL"));
+    }
 
 	@Test
 	void contextLoads() {
