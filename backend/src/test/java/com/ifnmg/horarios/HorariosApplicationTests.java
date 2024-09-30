9df75864903bf546367143a104bc37c442dfc03e
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2024-09-30 00:34:06 -0300

    Ajustes Finais para Deploy

--- backend/src/test/java/com/ifnmg/horarios/HorariosApplicationTests.java
+++ backend/src/test/java/com/ifnmg/horarios/HorariosApplicationTests.java
@@ -1,20 +1,13 @@
 package com.ifnmg.horarios;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HorariosApplicationTests {
 
-    @BeforeAll
-    public static void setUp() {
-        System.setProperty("API_KEY", System.getProperty("API_KEY", "default_api_key"));
-        System.setProperty("SPREADSHEET_ID", System.getProperty("SPREADSHEET_ID", "default_spreadsheet_id"));
-        System.setProperty("FRONTEND_URL", System.getProperty("FRONTEND_URL", "default_frontend_url"));
-    }
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void contextLoads() {
-    }
 }
