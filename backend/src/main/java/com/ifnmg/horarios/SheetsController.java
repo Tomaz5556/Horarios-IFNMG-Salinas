5c8eb982ba055fda1399ad7c94898a5edfde3dcf
Tomaz5556 <tmb5@aluno.ifnmg.edu.br>
2024-10-10 22:21:16 -0300

    Ajustes no backend

--- backend/src/main/java/com/ifnmg/horarios/SheetsController.java
+++ backend/src/main/java/com/ifnmg/horarios/SheetsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.text.Collator;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -437,16 +436,15 @@ public class SheetsController {
         String rangeMedio2 = "Horário - Ensino Médio!C3:V71";
         String rangeMedioTurmas = "Horário - Ensino Médio!C16:V16";
 
-        List<String> salas = Arrays.asList(
-            "(1/3)", "(1/4)", "(1/5)", "(1/6)", "(1/7)", "(1/8)", "(1/9)", "(1/10)", "(1/11)", "(1/12)",
-            "(1/13)", "(1/14)", "(1/15)", "(1/16)", "(1/17)", "(2/1)", "(2/2)", "(2/3)", "(2/4)", "(2/5)", "(2/6)",
-            "(3/7)", "(3/8)", "(3/9)", "(3/10)", "(3/11)", "(Agricult. I)", "(Agricult. II)", "(Agricult. III)",
-            "(Agroin. 1)", "(Agroin. 2)", "(Anexo Lab. Solos)", "(CELIN 1)", "(CELIN 2)", "(HV 1)", "(HV 2)", 
-            "(HV 3)", "(HV 4)", "(Lab. 1 - Info)", "(Lab. 2 - Info)", "(Lab. 3 - Info)", "(Lab. 4 - Info)",
-            "(Lab. Bromatologia)", "(Lab. Fenôm. de Transportes)", "(Lab. Física)", "(Lab. Invertebrados)", 
-            "(Lab. Microscopia)", "(Lab. Química I)", "(Lab. Química II)", "(Lab. Redes)", "(LEM)", "(Mini 1)", 
-            "(Mini 2)", "(Sala de Topografia)", "(Sala Suinocultura)", "(Sl. Análise Sensorial)", "(Zoo I)", "(Zoo II)", 
-            "(Zoo III)"
+        List<String> salas = List.of(
+            "(1/3)", "(1/4)", "(1/5)", "(1/6)", "(1/7)", "(1/8)", "(1/9)", "(1/10)", "(1/11)", "(1/12)", "(1/13)", "(1/14)", 
+            "(1/15)", "(1/16)", "(1/17)", "(2/1)", "(2/2)", "(2/3)", "(2/4)", "(2/5)", "(2/6)", "(3/7)", "(3/8)", "(3/9)", 
+            "(3/10)", "(3/11)", "(Agricult. I)", "(Agricult. II)", "(Agricult. III)", "(Agroin. 1)", "(Agroin. 2)", 
+            "(Anexo Lab. Solos)", "(CELIN 1)", "(CELIN 2)", "(HV 1)", "(HV 2)", "(HV 3)", "(HV 4)", "(Lab. 1 - Info)", 
+            "(Lab. 2 - Info)", "(Lab. 3 - Info)", "(Lab. 4 - Info)", "(Lab. Bromatologia)", "(Lab. Fenôm. de Transportes)", 
+            "(Lab. Física)", "(Lab. Invertebrados)", "(Lab. Microscopia)", "(Lab. Química I)", "(Lab. Química II)", 
+            "(Lab. Redes)", "(LEM)", "(Mini 1)", "(Mini 2)", "(Sala de Topografia)", "(Sala Suinocultura)", 
+            "(Sl. Análise Sensorial)", "(Zoo I)", "(Zoo II)", "(Zoo III)"
         );
 
         List<List<Object>> valuesSuperior1 = sheetsService.getSheetValues(rangeSuperior1);
